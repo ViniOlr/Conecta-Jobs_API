248c828277fc34b406a3ec5c961b9ad62894a1b5
Vinicius Oliveira <vinioliveiracontato03@gmail.com>
2023-10-04 23:55:41 -0300

    Fazendo conexao com o banco

--- index.py
+++ index.py
@@ -1,15 +1,49 @@
 from flask import Flask
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, flash
 from bardapi import Bard
 from key import  API_KEY_BARD
 import os
+import oracledb
 
 app = Flask(__name__)
 app.secret_key = 'conect-jobs'
 
+def conectaBanco():
+    try:
+        username = 'rm93613'
+        password = '150503'
+        dsn = 'oracle.fiap.com.br:1521/orcl'
+
+        # Crie uma conexão
+        conn = oracledb.connect(user="rm93613", password="150503", dsn="oracle.fiap.com.br:1521/orcl")
+
+        # Cria as instruções para cada módulo
+        inst_cadastro = conn.cursor()
+        inst_consulta = conn.cursor()
+        inst_alteracao = conn.cursor()
+        inst_exclusao = conn.cursor()
+
+        return[conn, inst_cadastro, inst_consulta, inst_alteracao, inst_exclusao]
+    except Exception as erro:
+        return['Erro', str(erro)]
+    
 @app.route('/testeVocacao', methods=['POST'])
 def testeVocacao():
     try:
+        conexaoComBanco = False
+        connection = ''
+        cursor = ''
+        
+        conexao = conectaBanco()
+
+        if conexao[0] == 'Erro':
+            # flash(f'Erro ao se conectar com o 213', 'error')
+            return 'Erro ao conectar com o banco de dados'
+        else:
+            connection = conexao[0]
+            cursor = conexao[1]
+            conexaoComBanco = True
+
         data = request.json
         nome = data.get('nome')
         
@@ -19,10 +53,25 @@ def testeVocacao():
 
         bard_output = Bard().get_answer(pergunta)['content']
 
-        return jsonify({"sucesso":bard_output})
-    except Exception as erro:
+        # sql = 'INSERT INTO cj_teste_vocacional (id_teste, id_usuario, pergunta, resposta)'
+        # sql += 'VALUES(seq_cj_id_teste.NEXTVAL, seq_cj_id_usuario.CURRVAL, :1, :2)'
 
-        return jsonify({'erro':erro})
+        # cursor.execute(sql, (pergunta, bard_output))
+
+    except Exception as erro:
+        if conexaoComBanco:
+            connection.rollback()
+
+        return jsonify({'erro':str(erro)})
+    else:
+        if conexaoComBanco:
+            connection.commit()
+
+        return jsonify({"sucesso":bard_output})
+    finally:
+        if conexaoComBanco:
+            connection.close()
+            cursor.close()
 
 
 if __name__ == '__main__':
